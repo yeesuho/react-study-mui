261a4b6a729fdf22bc8d77916bb3162ad72cdce3
yeesuho <suho080214@gmail.com>
2024-09-24 13:35:37 +0900

    feat: counter 추가

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,9 +1,18 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const [count, setCount] = useState(0);
+
+  function handleButtonClick() {
+    setCount(prev => prev + 1);
+  }
+
   return (
     <Stack>
       <Typography>Index Page</Typography>
+      <Typography variant='h1'>{count}</Typography>
+      <Button onClick={handleButtonClick} variant='contained'>Count</Button>
     </Stack>
   );
 }
